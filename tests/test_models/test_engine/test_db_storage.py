93649123b385eca41f18b705b7910fd3e1ddbd50
BlightG <birhane.gulilat@gmail.com>
2023-02-17 22:08:52 +0300

    added tests

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -90,7 +90,25 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_get(self):
         """Test that properly gets objects to file.json"""
+        new_state = State(name="New York")
+        new_state.save()
+        new_user = User(email="bob@foobar.com", password="password")
+        new_user.save()
+        self.assertIs(new_state, models.storage.get("State", new_state.id))
+        self.assertIs(None, models.storage.get("State", "blah"))
+        self.assertIs(None, models.storage.get("blah", "blah"))
+        self.assertIs(new_user, models.storage.get("User", new_user.id))
 
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_counts(self):
         """Test that properly counts objects to file.json"""
+        initial_count = models.storage.count()
+        intital_state_count = models.storage.count("State")
+        self.assertEqual(models.storage.count("Non_existent_class"), 0)
+        new_state = State(name="Adama")
+        new_state.save()
+        new_user = User(email="abe@abekebe.com", password="password")
+        new_user.save()
+        self.assertEqual(models.storage.count(
+            "State"), intital_state_count + 1)
+        self.assertEqual(models.storage.count(), initial_count + 2)
